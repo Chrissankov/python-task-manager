834da2fb064318e350b3aa069ff91ea120e29644
Christian Tannoury <tsmchris42@gmail.com>
2025-06-27 13:08:02 +0300

    Third Commit: Search for Task Function

--- task-manager.py
+++ task-manager.py
@@ -28,7 +28,45 @@ def complete_next_task(queue: list):
 
     return queue.pop(highest_priority_index)
 
+# Using Binary Search Algorithm
+def search_for_task(queue: list, title: str):
+    if is_empty(queue):
+        return "This title is for sure isn't in the tasks. Queue is EMPTY."
+
+    # Taking the titles only from the queue
+    titles = [[task[0], i] for i, task in enumerate(queue)]
+
+    # Insertion Sort
+    for i in range(1, len(titles)):
+        j = i
+
+        while j > 0 and titles[j - 1] > titles[j]:
+            titles[j - 1], titles[j] = titles[j], titles[j-1]
+            j -= 1
+
+    # Binary Search
+    low = 0
+    high = len(titles) -1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if titles[mid][0] == title:
+            return "Task with title '" + title +"' found at index " + str(titles[mid][1]) + "."
+        
+        elif titles[mid][0] < title:
+            low = mid + 1
+
+        elif titles[mid][0] > title:
+            high = mid - 1
+
+    return "Task with title '" + title + "' not found."
+
+
+
 tasks = []
+priorities= []
+
 
 # Get number of tasks from user
 num_tasks = int(input("Number of Tasks: "))
@@ -44,14 +82,16 @@ for i in range(num_tasks):
     duration = int(input("Enter duration (in minutes): "))
     while duration < 0:
         duration = int(input("Duration mustn't be negative: "))
-    
+
     priority = int(input("Enter priority (lower is higher): "))
-    while priority < 0:
-        priority = int(input("Priority mustn't be negative: "))
+
+    while priority < 0 or priority in priorities:
+        priority = int(input("Priority mustn't be negative and must be unique: "))
+
+    priorities.append(priority)
 
     insert_task(tasks, [title, duration, priority])
 
-# Show all tasks in queue
 print("\nAll Tasks in Queue: " + str(tasks))
 
 print("\nCompleted Task: " + str(complete_next_task(tasks)))
@@ -59,3 +99,4 @@ print("\nCompleted Task: " + str(complete_next_task(tasks)))
 print("\nAll Tasks in Queue: " + str(tasks))
 
 
+print(search_for_task(tasks,"Foo"))
